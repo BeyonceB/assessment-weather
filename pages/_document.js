07f2463e0dac32d1b2a3dbed8ad1de962a826d79
= <bbrowne14@my.bcit.ca>
2023-02-25 18:00:22 -0800

    assessment1

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,9 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=Karla:wght@400;500;600;700&family=Tilt+Warp&display=swap" rel="stylesheet" />
         <Main />
         <NextScript />
       </body>
